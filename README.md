7f411a33bc16a155586b7f5e8487de5522008057
Frank Jia <jiafrank98@gmail.com>
2019-04-24 11:35:07 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Clerc - Android
+
+### Color Schemes
+- Primary Light: #E0F2F1
+- Primary: #00796B
+- Primary Dark: #004C40
+- Accent: #FFB300
+- Dark Text: #212121
+- Light Text: #757575
+- White Text: #FFFFFF
+- Divider Color: #757575
+
+## License 
+Copyright © 2019 Clerc Technology Inc, All rights reserved
---
+++ app/src/main/java/com/paywithclerc/paywithclerc/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.paywithclerc.paywithclerc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        startShoppingButton.setOnClickListener {
+            Log.i("logtag", "Clicked!")
+        }
+    }
+}
