d8696499a585f53a7115bb7f3dfc71204e865935
Frank Jia <jiafrank98@gmail.com>
2019-05-26 06:56:53 -0700

    Merged hotfixes from release, switched to dev

--- app/src/main/java/com/paywithclerc/paywithclerc/constant/BackendConstants.kt
+++ app/src/main/java/com/paywithclerc/paywithclerc/constant/BackendConstants.kt
@@ -2,7 +2,7 @@ package com.paywithclerc.paywithclerc.constant
 
 object BackendConstants {
 
-    const val BACKEND_URL = "https://paywithclerc.appspot.com"
+    const val BACKEND_URL = "https://paywithclerc-dev.appspot.com"
     const val CREATE_CUST_URL = "$BACKEND_URL/customers/create"
     const val CREATE_EPH_KEY_URL = "$BACKEND_URL/customers/create-ephemeral-key"
     const val JWT_URL = "$BACKEND_URL/jwt/refresh"
