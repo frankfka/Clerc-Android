c5aed770400108adaabe9c4d9f663c818575767b
Frank Jia <31530056+frankfka@users.noreply.github.com>
2019-10-17 19:49:04 -0700

    Update StripeConstants.kt

--- app/src/main/java/com/paywithclerc/paywithclerc/constant/StripeConstants.kt
+++ app/src/main/java/com/paywithclerc/paywithclerc/constant/StripeConstants.kt
@@ -2,7 +2,7 @@ package com.paywithclerc.paywithclerc.constant
 
 object StripeConstants {
 
-    const val PUBLISHABLE_KEY = "pk_test_LvuvRJTzvgVmxMHSFFnz5VqR00DCZ68QK6"
+    const val PUBLISHABLE_KEY = ""
     const val DEFAULT_CURRENCY = "cad"
 
 }
